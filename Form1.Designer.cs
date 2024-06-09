5d11190722a623449f60a0fc72e75071123a2355
Aleksandr.Popov <aa.popov@i-teco.ru>
2024-06-10 00:50:51 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ib_read = new Button();
+            this.ub_read = new Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.spektr = new Button();
+            this.power = new Button();
+            this.pathLabel = new Label();
+            this.pathBox = new TextBox();
+            this.durationBox = new TextBox();
+            this.durationLabel = new Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ib_read
+            // 
+            this.ib_read.Location = new System.Drawing.Point(30, 20);
+            this.ib_read.BackColor = Color.LightGreen;
+            this.ib_read.ForeColor = Color.DarkBlue;
+            this.ib_read.Name = "ib_read";
+            this.ib_read.Size = new System.Drawing.Size(150, 50);
+            this.ib_read.TabIndex = 0;
+            this.ib_read.Text = "Чтение данных IB";
+            this.ib_read.UseVisualStyleBackColor = false;
+            this.ib_read.Click += new System.EventHandler(this.ib_read_Click);
+            // 
+            // ub_read
+            // 
+            this.ub_read.Location = new System.Drawing.Point(200, 20);
+            this.ub_read.BackColor = Color.LightSkyBlue;
+            this.ub_read.ForeColor = Color.DarkBlue;
+            this.ub_read.Name = "ub_read";
+            this.ub_read.Size = new System.Drawing.Size(150, 50);
+            this.ub_read.TabIndex = 1;
+            this.ub_read.Text = "Чтение данных UB";
+            this.ub_read.UseVisualStyleBackColor = false;
+            this.ub_read.Click += new System.EventHandler(this.ub_read_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chartArea1.BackColor = Color.White;
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            //this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(30, 150);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1300, 700);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // spektr
+            // 
+            this.spektr.Location = new System.Drawing.Point(370, 20);
+            this.spektr.BackColor = Color.LightSalmon;
+            this.spektr.ForeColor = Color.DarkBlue;
+            this.spektr.Name = "spektr";
+            this.spektr.Size = new System.Drawing.Size(150, 50);
+            this.spektr.TabIndex = 3;
+            this.spektr.Text = "Спектр";
+            this.spektr.UseVisualStyleBackColor = false;
+            this.spektr.Click += new System.EventHandler(this.spektr_Click);
+            // 
+            // power
+            // 
+            this.power.Location = new System.Drawing.Point(540, 20);
+            this.power.BackColor = Color.LightCoral;
+            this.power.ForeColor = Color.DarkBlue;
+            this.power.Name = "power";
+            this.power.Size = new System.Drawing.Size(150, 50);
+            this.power.TabIndex = 4;
+            this.power.Text = "Мгновенная мощность";
+            this.power.UseVisualStyleBackColor = false;
+            this.power.Click += new System.EventHandler(this.power_Click);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.Location = new System.Drawing.Point(30, 90);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(150, 22);
+            this.pathLabel.TabIndex = 5;
+            this.pathLabel.Text = "Путь к файлу:";
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(200, 90);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(490, 22);
+            this.pathBox.TabIndex = 6;
+            // 
+            // durationLabel
+            // 
+            this.durationLabel.Location = new System.Drawing.Point(30, 120);
+            this.durationLabel.Name = "durationLabel";
+            this.durationLabel.Size = new System.Drawing.Size(150, 22);
+            this.durationLabel.TabIndex = 7;
+            this.durationLabel.Text = "Продолжительность:";
+            // 
+            // durationBox
+            // 
+            this.durationBox.Location = new System.Drawing.Point(200, 120);
+            this.durationBox.Name = "durationBox";
+            this.durationBox.Size = new System.Drawing.Size(150, 22);
+            this.durationBox.TabIndex = 8;
+
+
+            this.ib_read.Location = new System.Drawing.Point(30, 20);
+            this.ub_read.Location = new System.Drawing.Point(30, 80);
+            this.spektr.Location = new System.Drawing.Point(30, 140);
+            this.power.Location = new System.Drawing.Point(30, 200);
+
+            this.pathLabel.Location = new System.Drawing.Point(30, 260);
+            this.pathLabel.Size = new System.Drawing.Size(150, 22);
+            this.pathBox.Location = new System.Drawing.Point(30, 290);
+            this.pathBox.Size = new System.Drawing.Size(250, 22);
+
+            this.durationLabel.Location = new System.Drawing.Point(30, 330);
+            this.durationLabel.Size = new System.Drawing.Size(150, 22);
+            this.durationBox.Location = new System.Drawing.Point(30, 360);
+            this.durationBox.Size = new System.Drawing.Size(150, 22);
+
+            this.chart1.Location = new System.Drawing.Point(300, 20);
+            this.chart1.Size = new System.Drawing.Size(1000, 630);
+            this.chart1.Series[0].Color = Color.Red; // Красная линия
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 720);
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.power);
+            this.Controls.Add(this.spektr);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.ub_read);
+            this.Controls.Add(this.ib_read);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.durationLabel);
+            this.Controls.Add(this.durationBox);
+            this.Name = "Form1";
+            this.Text = "Анализ данных";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+
+        }
+
+        #endregion
+        private Label pathLabel;
+        private Label durationLabel;
+        private TextBox pathBox;
+        private TextBox durationBox;
+        private System.Windows.Forms.Button ib_read;
+        private System.Windows.Forms.Button ub_read;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button spektr;
+        private System.Windows.Forms.Button power;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using FftSharp;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void PlotGraph(string graphType)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                SetFilePath(openFileDialog.FileName);
+                SetDuration(openFileDialog.FileName);
+                switch (graphType)
+                {
+                    case "Напряжение":
+                        napryazhenie(fileName, 332);
+                        break;
+                    case "Сила тока":
+                        sila_toka(fileName, 332);
+                        break;
+                    case "Спектр":
+                        CalculateSpectrum(fileName, 332);
+                        break;
+                    default:
+                        MessageBox.Show("Неверный тип графика.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+        }
+        private void power_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string voltageFileName = openFileDialog1.FileName;
+                SetFilePath(openFileDialog1.FileName);
+                SetDuration(openFileDialog1.FileName);
+                OpenFileDialog openFileDialog2 = new OpenFileDialog();
+
+                if (openFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    string currentFileName = openFileDialog2.FileName;
+                    CalculatePower(voltageFileName, currentFileName, 332);
+                }
+            }
+        }
+        private void ib_read_Click(object sender, EventArgs e)
+        {
+            PlotGraph("Сила тока");
+        }
+        private void ub_read_Click(object sender, EventArgs e)
+        {
+            PlotGraph("Напряжение");
+        }
+        private void spektr_Click(object sender, EventArgs e)
+        {
+            PlotGraph("Спектр");
+        }
+        private void sila_toka(string fileName, int lineNumber)
+        {
+            PlotChart(fileName, "Зависимость силы тока от времени", "Time (seconds)", "I(t)", 0.00125, lineNumber);
+        }
+
+        private void napryazhenie(string fileName, int lineNumber)
+        {
+            PlotChart(fileName, "Зависимость напряжения от времени", "Time (seconds)", "U(t)", 0.00125, lineNumber);
+        }
+
+        private void PlotChart(string fileName, string chartTitle, string xAxisTitle, string yAxisTitle, double interval, int lineNumber)
+        {
+
+            double currentTime = 0;
+            // Чтение данных из файла
+            string[] lines = File.ReadAllLines(fileName);
+
+            if (lineNumber >= lines.Length || lineNumber < 0)
+            {
+                MessageBox.Show("Номер строки выходит за пределы файла.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string line = lines[lineNumber];
+            string[] values = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); // Разделение строки на отдельные значения по пробелу
+
+            // Очистка старых данных графика
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            // Создание новой области для графика
+            ChartArea chartArea = new ChartArea();
+            chart1.ChartAreas.Add(chartArea);
+
+            // Создание нового ряда данных
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 4; // Установка толщины линии
+            chart1.Series.Add(series);
+
+            // Добавление данных в ряд
+            foreach (string value in values)
+            {
+                double parsedValue;
+                if (double.TryParse(value.Replace('.', ','), out parsedValue))
+                {
+                    series.Points.AddXY(currentTime, parsedValue);
+                    currentTime += interval;
+                }
+                else
+                {
+                    MessageBox.Show($"Ошибка при считывании данных: Не удалось преобразовать значение \"{value}\" в число.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return; // Прерываем выполнение метода при возникновении ошибки
+                }
+            }
+
+            // Настройка масштаба и легенды
+            chartArea.AxisX.Title = xAxisTitle;
+            chartArea.AxisY.Title = yAxisTitle;
+            chartArea.AxisX.MajorGrid.Enabled = true;
+            chartArea.AxisY.MajorGrid.Enabled = true;
+            chartArea.AxisX.Interval = interval; // Интервал между отметками по оси X
+
+            double minX = series.Points.Min(p => p.XValue);
+
+            // Установка названия графика
+            chart1.Titles.Clear();
+            chart1.Titles.Add(chartTitle);
+            chart1.Legends.Clear();
+            chart1.Legends.Add(new Legend("Legend"));
+            chartArea.AxisX.Minimum = minX;
+            chartArea.AxisX.Interval = interval;
+            chart1.Series[0].Legend = "Legend";
+            chart1.Series[0].IsVisibleInLegend = true;
+            chart1.Series[0].Color = Color.Red;
+
+        }
+        private void CalculateSpectrum(string fileName, int lineNumber)
+        {
+
+            // Чтение данных из файла
+            string[] lines = File.ReadAllLines(fileName);
+
+            if (lineNumber >= lines.Length || lineNumber < 0)
+            {
+                MessageBox.Show("Номер строки выходит за пределы файла.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string line = lines[lineNumber];
+            string[] values = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Преобразование строковых значений в double
+            double[] signal = new double[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                double.TryParse(values[i].Replace('.', ','), out signal[i]);
+            }
+            double[] spectrum = FftSharp.Transform.FFTmagnitude(signal);
+            double[] frequencies = new double[spectrum.Length];
+            for (int i = 0; i < spectrum.Length; i++)
+            {
+                // Вычисляем частоту по формуле 800 / (64 * i)
+                frequencies[i] = (800 / 64) * (i + 1);
+            }
+
+            // Очистка старых данных графика
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            // Создание новой области для графика
+            ChartArea chartArea = new ChartArea();
+            chart1.ChartAreas.Add(chartArea);
+
+            // Создание нового ряда данных
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 4; // Установка толщины линии
+            chart1.Series.Add(series);
+            for (int i = 0; i < spectrum.Length; i++)
+            {
+                series.Points.AddXY(frequencies[i], spectrum[i]);
+            }
+            double minX = series.Points.Min(p => p.XValue);
+            chartArea.AxisX.Minimum = minX;
+            chartArea.AxisX.Title = "Частота";
+            chartArea.AxisY.Title = "Амплитуда Спектра";
+            chartArea.AxisX.MajorGrid.Enabled = true;
+            chartArea.AxisY.MajorGrid.Enabled = true;
+            chartArea.AxisX.Interval = 8; // Интервал между отметками по оси X
+          
+
+            chart1.Titles.Clear();
+            chart1.Titles.Add("Спектр");
+            chart1.Legends.Clear();
+            chart1.Legends.Add(new Legend("Legend1"));
+            chart1.Series[0].Legend = "Legend1";
+            chart1.Series[0].IsVisibleInLegend = true;
+
+        }
+        private void CalculatePower(string voltageFileName, string currentFileName, int lineNumber)
+        {
+            double current_time = 0;
+            double intetval = 0.00125;
+
+            string[] voltageLines = File.ReadAllLines(voltageFileName);
+            string[] amperageLines = File.ReadAllLines(currentFileName);
+            // Проверка корректности номера строки
+            if (lineNumber >= voltageLines.Length || lineNumber >= amperageLines.Length || lineNumber < 0)
+            {
+                MessageBox.Show("Номер строки выходит за пределы файла.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Чтение значения напряжения из строки
+            string voltageLine = voltageLines[lineNumber];
+            string[] voltageValues = voltageLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            double[] voltages = new double[voltageValues.Length];
+            for (int i = 0; i < voltageValues.Length; i++)
+            {
+                double.TryParse(voltageValues[i].Replace('.', ','), out voltages[i]);
+            }
+
+            // Чтение значения силы тока из строки
+            string currentLine = amperageLines[lineNumber];
+            string[] currentValues = currentLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            double[] currents = new double[currentValues.Length];
+            for (int i = 0; i < currentValues.Length; i++)
+            {
+                double.TryParse(currentValues[i].Replace('.', ','), out currents[i]);
+            }
+
+            // Проверка совпадения количества значений напряжения и силы тока
+            if (voltages.Length != currents.Length)
+            {
+                MessageBox.Show("Количество значений напряжения и силы тока не совпадает.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Вычисление мощности
+            double[] powers = new double[voltages.Length];
+            for (int i = 0; i < voltages.Length; i++)
+            {
+                powers[i] = voltages[i] * currents[i];
+            }
+
+            // Очистка старых данных графика
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            // Создание новой области для графика
+            ChartArea chartArea = new ChartArea();
+            chart1.ChartAreas.Add(chartArea);
+
+            // Создание нового ряда данных для мощности
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 4; // Установка толщины линии
+            chart1.Series.Add(series);
+
+            // Добавление точек на график
+            for (int i = 0; i < powers.Length; i++)
+            {
+                series.Points.AddXY(current_time, powers[i]);
+                current_time += intetval;
+            }
+            
+            // Настройка масштаба и легенды
+            chartArea.AxisX.Title = "Time (seconds)";
+            chartArea.AxisY.Title = "Power";
+            chartArea.AxisX.MajorGrid.Enabled = true;
+            chartArea.AxisY.MajorGrid.Enabled = true;
+            chartArea.AxisX.Interval = 1; // Интервал между отметками по оси X
+            double minX = series.Points.Min(p => p.XValue);
+            chart1.Titles.Clear();
+            chart1.Titles.Add("Power");
+            chart1.Legends.Clear();
+            chartArea.AxisX.Minimum = minX;
+            chartArea.AxisX.Interval = intetval;
+            chart1.Legends.Add(new Legend("Legend1"));
+            chart1.Series[0].Legend = "Legend1";
+            chart1.Series[0].IsVisibleInLegend = true;
+        }
+        private void SetFilePath(string filePath)
+        {
+            this.pathBox.Text = filePath;
+        }
+        private void SetDuration(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            int duration = lines.Length / 10;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(duration);
+            this.durationBox.Text = $"{timeSpan.Minutes}:{timeSpan.Seconds}";
+        }
+    }
+}
